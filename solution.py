a65a7cfa11a49216f8297380b6254c34d8bdeab8
Jakub Klikowski <abuk9999@gmail.com>
2018-11-13 10:09:31 +0100

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,26 @@
+# Exercise 1
+# Fill with code
+
+
+# Exercise 2
+# Fill with code
+
+
+# Exercise 3
+# Fill with code
+
+
+# Exercise 4
+# Fill with code
+
+
+# Exercise 5
+# Fill with code
+
+
+# Exercise 6
+def prepare_data(file_name):
+
+    # Fill with code
+
+    return X, y
